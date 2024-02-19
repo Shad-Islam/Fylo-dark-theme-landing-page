4459d5b9ecf4f190dd5b75e65808cd075b093020
Shad-islam <shadislam48@gmail.com>
2024-02-19 23:50:03 +0600

    Add global style corectly

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from "./components/Navbar";
+import Global from "./components/styles/globalStyle";
 
 function App() {
   return (
     <>
+      <Global />
       <Navbar />
     </>
   );
---
+++ src/components/styles/globalStyle.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+const Global = createGlobalStyle`
+body{
+background-color: aqua;
+height: 100vh;
+width: 100%;
+}
+
+`;
+
+export default Global;
