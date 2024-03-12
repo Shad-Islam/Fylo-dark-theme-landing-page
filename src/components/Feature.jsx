2302ac8b5f3208ffa7b12cd62b2b395babab8763
Shad-islam <m.shadislam01@gmail.com>
2024-03-13 02:54:04 +0600

    complete feature section design

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,21 +1,80 @@
 import React from "react";
 import {
   FeaturesRow,
-  FeaturesContainer,
   FeaturesItem,
+  FeaturesItemText,
+  FeaturesItemImage,
+  FeaturesContainer,
+  FeaturesItemTitle,
+  FeaturesItemIcon,
 } from "./styles/feature.style";
+
+import iconAnyFile from "../images/icon-any-file.svg";
+import iconSecurity from "../images/icon-security.svg";
+import iconCollaboration from "../images/icon-collaboration.svg";
+import iconAccessAnywhere from "../images/icon-access-anywhere.svg";
 
 function Feature() {
   return (
     <>
       <FeaturesContainer>
         <FeaturesRow>
-          <FeaturesItem></FeaturesItem>
-          <FeaturesItem></FeaturesItem>
+          {/* item 1 */}
+          <FeaturesItem>
+            <FeaturesItemIcon>
+              <FeaturesItemImage src={iconAccessAnywhere} />
+            </FeaturesItemIcon>
+
+            <FeaturesItemTitle>Access your files, anywhere</FeaturesItemTitle>
+            <FeaturesItemText>
+              The ability to use a smartphone, tablet, or computer
+              <br />
+              to access your account means your files follow you
+              <br />
+              everywhere.
+            </FeaturesItemText>
+          </FeaturesItem>
+          {/* item 2 */}
+          <FeaturesItem>
+            <FeaturesItemIcon>
+              <FeaturesItemImage src={iconSecurity} />
+            </FeaturesItemIcon>
+            <FeaturesItemTitle> Security you can trust </FeaturesItemTitle>
+            <FeaturesItemText>
+              2-factor authentication and user-controlled encryption are
+              <br />
+              just a couple of the security features we allow to help
+              <br />
+              secure your files.
+            </FeaturesItemText>
+          </FeaturesItem>
         </FeaturesRow>
+
         <FeaturesRow>
-          <FeaturesItem></FeaturesItem>
-          <FeaturesItem></FeaturesItem>
+          {/* item 3  */}
+          <FeaturesItem>
+            <FeaturesItemImage src={iconCollaboration} />
+            <FeaturesItemTitle>Real-time collaboration</FeaturesItemTitle>
+            <FeaturesItemText>
+              Securely share files and folders with friends, family and
+              <br />
+              colleagues for live collaboration. No email attachments
+              <br />
+              required.
+            </FeaturesItemText>
+          </FeaturesItem>
+          {/* item 4 */}
+          <FeaturesItem>
+            <FeaturesItemImage src={iconAnyFile} />
+            <FeaturesItemTitle>Store any type of file</FeaturesItemTitle>
+            <FeaturesItemText>
+              Whether you're sharing holidays photos or work
+              <br />
+              documents, Fylo has you covered allowing for all file
+              <br />
+              types to be securely stored and shared.
+            </FeaturesItemText>
+          </FeaturesItem>
         </FeaturesRow>
       </FeaturesContainer>
     </>
--- src/components/styles/feature.style.js
+++ src/components/styles/feature.style.js
@@ -3,23 +3,60 @@ import styled from "styled-components";
 const FeaturesContainer = styled.div`
   width: 100%;
   height: 700px;
-  background-color: #fff;
+  /* background-color: #fff; */
 `;
 
 const FeaturesRow = styled.div`
   width: 100%;
   height: 350px;
-  background-color: #f1f1f1;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+
+  /* background-color: #f1f1f1; */
 
   &:last-child {
-    background-color: rebeccapurple;
+    /* background-color: rebeccapurple; */
+
   }
 `;
 
 const FeaturesItem = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: blue;
+  width: 500px;
+  height: 300px;
+  padding: 30px;
+  text-align: center;
+  /* background-color: blue; */
+
+  & > * {
+    margin-bottom: 20px;
+  }
 `;
 
-export { FeaturesRow, FeaturesItem, FeaturesContainer };
+const FeaturesItemIcon = styled.div`
+  height: 80px;
+
+  img {
+    height: 100%;
+  }
+`;
+
+const FeaturesItemImage = styled.img`
+  /* height: 78px; */
+`;
+const FeaturesItemTitle = styled.h2`
+  color: hsl(0, 0%, 100%);
+`;
+const FeaturesItemText = styled.p`
+  color: hsl(0, 0%, 90%);
+`;
+
+export {
+  FeaturesRow,
+  FeaturesItem,
+  FeaturesItemText,
+  FeaturesItemImage,
+  FeaturesItemTitle,
+  FeaturesItemIcon,
+  FeaturesContainer,
+};
