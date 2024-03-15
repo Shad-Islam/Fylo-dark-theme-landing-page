5e9fc1b521a09514d63b625cabc9399208aac3ec
Shad-islam <m.shadislam01@gmail.com>
2024-03-15 07:54:22 +0600

    Design the description section

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import {
+  DescriptionText,
+  DescriptionLink,
   DescriptionImage,
+  DescriptionHeading,
+  DescriptionTextPart,
   DescriptionContainer,
-  DescriptionTextContent,
+  DescriptionImagePart,
 } from "./styles/description.style";
 import StayProductive from "../images/illustration-stay-productive.png";
 
@@ -10,8 +14,29 @@ function Description() {
   return (
     <>
       <DescriptionContainer>
-        <DescriptionImage src={StayProductive} />
-        <DescriptionTextContent></DescriptionTextContent>
+        <DescriptionImagePart>
+          <DescriptionImage src={StayProductive} />
+        </DescriptionImagePart>
+        <DescriptionTextPart>
+          <DescriptionHeading>
+            Stay productive,
+            <br />
+            wherever you are
+          </DescriptionHeading>
+          <DescriptionText>
+            Never let location be an issue when accessing your files. Fylo has
+            you
+            <br />
+            covered for all of your file storage needs.
+          </DescriptionText>
+          <DescriptionText>
+            Securely share files and folders with friends, family and colleagues
+            for live
+            <br />
+            collaboration. No email attachments required.
+          </DescriptionText>
+          <DescriptionLink href="#">See how Fylo works</DescriptionLink>
+        </DescriptionTextPart>
       </DescriptionContainer>
     </>
   );
--- src/components/styles/description.style.js
+++ src/components/styles/description.style.js
@@ -1,8 +1,46 @@
 import styled from "styled-components";
 
-const DescriptionContainer = styled.div``;
+const DescriptionContainer = styled.div`
+  display: flex;
+  /* background-color: aqua; */
+  margin-top: 20px;
+  /* justify-content: space-around; */
+`;
 
+const DescriptionImagePart = styled.div`
+  width: 50%;
+  /* padding-left: 20px; */
+  /* background-color: blueviolet; */
+`;
 const DescriptionImage = styled.img``;
-const DescriptionTextContent = styled.div``;
+const DescriptionTextPart = styled.div`
+  gap: 20px;
+  width: 50%;
+  display: flex;
+  padding-left: 30px;
+  /* background-color: red; */
+  flex-direction: column;
+  justify-content: center;
+`;
+const DescriptionHeading = styled.h2`
+  font-size: 30px;
+  color: hsl(0, 0%, 100%);
+  /* background-color: bisque; */
+`;
+const DescriptionText = styled.p`
+  color: hsl(0, 0%, 85%);
+`;
+const DescriptionLink = styled.a`
+  text-decoration: none;
+  color: hsl(176, 68%, 64%);
+`;
 
-export { DescriptionImage, DescriptionContainer, DescriptionTextContent };
+export {
+  DescriptionImagePart,
+  DescriptionText,
+  DescriptionLink,
+  DescriptionImage,
+  DescriptionHeading,
+  DescriptionTextPart,
+  DescriptionContainer,
+};
