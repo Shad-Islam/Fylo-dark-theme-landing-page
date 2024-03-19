cda57f37bf4d589ac670d2cdd13a025fc9994bed
Shad-islam <m.shadislam01@gmail.com>
2024-03-19 17:14:39 +0600

    Add new footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FooterArea } from "./styles/footer.style";
+
+function Footer() {
+  return <FooterArea></FooterArea>;
+}
+
+export default Footer;
---
+++ src/components/styles/footer.style.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const FooterArea = styled.div`
+  width: 100%;
+  background-color: hsl(216, 53%, 9%);
+`;
+
+export { FooterArea };
