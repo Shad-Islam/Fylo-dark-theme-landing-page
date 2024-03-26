b31578b27f7fb49aca2ca7c802fc9ae6f90eac91
Shad-islam <m.shadislam01@gmail.com>
2024-03-27 02:44:46 +0600

    Create structure of footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,62 @@
 import React from "react";
-import { FooterArea, FooterContainer } from "./styles/footer.style";
+import {
+  FooterNav,
+  FooterPhone,
+  FooterAddress,
+  FooterArea,
+  FooterCol,
+  FooterCols,
+  FooterContainer,
+  FooterLogo,
+  FooterSocialIcon,
+} from "./styles/footer.style";
+import logo from "../images/logo.svg";
 
 function Footer() {
   return (
     <>
       <FooterArea>
-        <FooterContainer></FooterContainer>
+        <FooterContainer>
+          <FooterLogo src={logo} />
+          <FooterCols>
+            {/* address */}
+            <FooterAddress>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore gagna aliqua
+            </FooterAddress>
+
+            {/* email and phone number  */}
+            <FooterPhone>
+              <p>+1-543-123-4567</p>
+              <p>examole@tvio.com</p>
+            </FooterPhone>
+
+            {/* quick nav  */}
+            <FooterNav>
+              <ul>
+                <li>About Us</li>
+                <li>Jobs</li>
+                <li>Press</li>
+                <li>Blog</li>
+              </ul>
+            </FooterNav>
+
+            {/* quick nav  */}
+            <FooterNav>
+              <ul>
+                <li>Contact Us</li>
+                <li>Terms</li>
+                <li>Privacy</li>
+              </ul>
+            </FooterNav>
+
+            <FooterSocialIcon>
+              
+            </FooterSocialIcon>
+
+            {/* social media link */}
+          </FooterCols>
+        </FooterContainer>
       </FooterArea>
     </>
   );
--- src/components/styles/footer.style.js
+++ src/components/styles/footer.style.js
@@ -6,7 +6,51 @@ const FooterArea = styled.div`
   background-color: hsl(216, 53%, 9%);
 `;
 const FooterContainer = styled.div`
+  height: 100%;
   width: 1440px;
+  background-color: aqua;
+  margin: 0 auto;
 `;
 
-export { FooterArea, FooterContainer };
+const FooterLogo = styled.img`
+  background-color: red;
+`;
+
+const FooterCols = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background-color: orange;
+`;
+const FooterCol = styled.div`
+  width: 100px;
+  background-color: blue;
+`;
+const FooterAddress = styled.div`
+  width: 300px;
+  background-color: blue;
+`;
+const FooterPhone = styled.div`
+  width: 200px;
+  background-color: blue;
+`;
+const FooterNav = styled.div`
+  width: 200px;
+  background-color: blue;
+`;
+const FooterSocialIcon = styled.div`
+  width: 200px;
+  background-color: blue;
+`;
+
+export {
+  FooterSocialIcon,
+  FooterNav,
+  FooterPhone,
+  FooterAddress,
+  FooterCols,
+  FooterCol,
+  FooterLogo,
+  FooterArea,
+  FooterContainer,
+};
